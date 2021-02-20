2f2ef1723c16fef5df041b2ac66b68d45ab16738
David "Fires" Stein <info@davidstein.cz>
2021-02-20 21:37:23 +0100

    Latest version edited to syringe

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@
 ezButton buttonRun(BUTTON_RUN);
 ezButton buttonUp(BUTTON_UP);
 ezButton buttonDown(BUTTON_DOWN);
+
 ezButton endstopBed(ENDSTOP_BED);
 ezButton endstopCart(ENDSTOP_CART);
 
@@ -25,14 +26,14 @@ ezButton endstopCart(ENDSTOP_CART);
  */
 #include "A4988.h"
 #define STEPPER_CART_STEPS 200
-#define STEPPER_CART_RPM 30
+#define STEPPER_CART_RPM 90
 #define STEPPER_CART_MICRO 8
 #define STEPPER_CART_DIR 7
 #define STEPPER_CART_STEP 8
 A4988 stepperCart(STEPPER_CART_STEPS,STEPPER_CART_DIR,STEPPER_CART_STEP);
 
 #define STEPPER_BED_STEPS 200
-#define STEPPER_BED_RPM 30
+#define STEPPER_BED_RPM 40
 #define STEPPER_BED_MICRO 8
 #define STEPPER_BED_DIR 9
 #define STEPPER_BED_STEP 10
@@ -42,10 +43,12 @@ A4988 stepperBed(STEPPER_BED_STEPS,STEPPER_BED_DIR,STEPPER_BED_STEP);
  * DC motors
  */
 #include "L298N.h"
-#define MOTOR_IN1 11
+#define MOTOR_ENABLE 11
+#define MOTOR_IN1 13
 #define MOTOR_IN2 12
 #define MOTOR_SPEED 255
-L298N motorPush(MOTOR_IN1,MOTOR_IN2);
+#define MOTOR_SPEED_EXTRUDE 55
+L298N motorPush(MOTOR_ENABLE,MOTOR_IN1,MOTOR_IN2);
 
 /**
  * Other variables and functions
@@ -60,6 +63,9 @@ void buttonRefresh(){
 
 void homeCart(){
   // home the cart
+  buttonRefresh();
+  stepperCart.move(250);
+  delay(10);
   stepperCart.startMove(-10000); //some steps to reach the endstop
   Serial.println("Stepper cart -> going to home");
   while(!endstopCart.isPressed()){
@@ -72,7 +78,8 @@ void homeCart(){
 
 void homeBed(){
   // home the bed
-  stepperBed.startMove(300); //some steps to reach the endstop
+  buttonRefresh();
+  stepperBed.startMove(3600); //some steps to reach the endstop
   Serial.println("Stepper bed -> going to home");
   while(!endstopBed.isPressed()){
       buttonRefresh();
@@ -140,11 +147,13 @@ void setup(){
   stepperCart.begin(STEPPER_CART_RPM,STEPPER_CART_MICRO);
 
   ///prepare motor
+  analogWrite(MOTOR_ENABLE,255);
   motorPush.setSpeed(MOTOR_SPEED);
 
 
   //SETUP RUTINE
-  homeCart();
+  stepperCart.move(250);
+  //homeCart();
   homeBed();
 }
 
@@ -152,27 +161,43 @@ void setup(){
 void loop(){
   buttonRefresh(); //refresh buttons
 
-  //check up/down buttons
-  if(buttonUp.isPressed() || buttonDown.isPressed()){
-      if(buttonUp.isPressed()){
-        motorPush.forward();
-      }else{
-        motorPush.backward();
-      }
-  }else{
-    motorPush.stop();
+  //check buttons
+  if(buttonUp.isPressed()){
+    Serial.println("MOTOR - UP");
+    while(!buttonUp.isReleased()){
+          //Serial.println("MOTOR - UP");
+          buttonRefresh();
+          motorPush.backward();
+    }
   }
+  if(buttonDown.isPressed()){
+    Serial.println("MOTOR - DOWN");
+    while(!buttonDown.isReleased()){
+          //Serial.println("MOTOR - DOWN");
+          buttonRefresh();
+          motorPush.forward();
+    }
+  }
+  motorPush.stop();
 
+  //run button
   if(buttonRun.isPressed()){
     Serial.println("RUN - start");
-    //stepperCart.move(-50); //cart to back possition
-    //motorPush.forward(); //start extrude
-    //stepperBed.rotate(360);
-    //motorPush.stop();
-    Serial.println("RUN - done, going to home");
     homeCart();
+    stepperCart.move(5325); //cart to back possition
+    motorPush.setSpeed(MOTOR_SPEED_EXTRUDE);
+    motorPush.forward(); //start extrude
+    stepperBed.rotate(360*2);
+    motorPush.stop();
+    stepperBed.rotate(360*1);
+    motorPush.setSpeed(MOTOR_SPEED);
+    motorPush.backward();
+    delay(100);
+    motorPush.stop();
+    Serial.println("RUN - done, going to home");
     increaseRunCounter();
     printRunCount();
+    homeCart();
     delay(200);
   }
 }
